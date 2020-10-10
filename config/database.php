9f40af5594dc6a476f05c73a735bb55775225326
samia007-create <59336422+samia007-create@users.noreply.github.com>
2020-10-10 16:31:17 +0100

    update databse

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('https://www.db4free.net/phpMyAdmin/db_structure.php?server=1&db=atmostechstore'),
-            'host' => env('DB_HOST', 'db4free.net/'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'atmostechstore'),
-            'username' => env('DB_USERNAME', 'samialou'),
-            'password' => env('DB_PASSWORD', 'e4180222'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
