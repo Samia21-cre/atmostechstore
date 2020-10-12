7d5212f3686b65b47bbbef4d4a44e04fc03d4571
samia007-create <59336422+samia007-create@users.noreply.github.com>
2020-10-12 13:31:02 +0100

    Add auth middleware

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -15,6 +15,15 @@ use App\Product;
 class AdminProductController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
